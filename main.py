f79d9444d5d490306573fd0e6e5193638e4827a2
Awan <155275944+GitHubAwan@users.noreply.github.com>
2024-05-22 16:19:59 -0700

    second commit

--- main.py
+++ main.py
@@ -6,19 +6,24 @@ BACKGROUND_COLOR = "#B1DDC6"
 
 
 class FlashcardApp:
-    def __init__(self, root):
+    def __init__(self, window):
         self.pkword= Pick_word()
-        self.root = root
-        self.root.title("Flashcard App")
-        self.root.config(padx=50,pady=50,bg=BACKGROUND_COLOR)
+        self.window = window
+        self.window.title("Flashcard App")
+        self.window.config(padx=50,pady=50,bg=BACKGROUND_COLOR)
+
+
 
         # 设置窗口大小
         # self.window.geometry("800x400")
 
+        self.flip_timer = self.window.after(3000,func=self.flip_card)
+
         self.canvas = Canvas(width=800, height=526)
         self.card_front_img = PhotoImage(file="images/card_front.png")
+        self.card_back_ing = PhotoImage(file="images/card_back.png")
         # self.logo_img = PhotoImage(file="images/card_front.png")
-        self.canvas.create_image(400, 263, image=self.card_front_img)
+        self.card_background = self.canvas.create_image(400, 263, image=self.card_front_img)
         # 创建文本并保存它们的ID
         self.title = self.canvas.create_text(400,150,text="Title",font=("Ariel",40,"italic"))
         self.word = self.canvas.create_text(400, 265, text="word", font=("Ariel", 60, "bold"))
@@ -34,10 +39,21 @@ class FlashcardApp:
         self.forget_image = PhotoImage(file="images/wrong.png")
         self.forget_button = Button(image=self.forget_image,text="aa", highlightthickness=0,command=self.pickword)
         self.forget_button.grid(column=2, row=1)
+        self.pickword()
 
     def pickword(self):
-        word = self.pkword.pick_random_word()
-        self.canvas.itemconfig(self.word,text=word)
+        self.window.after_cancel(self.flip_timer)
+        self.word_fr, self.word_en=self.pkword.pick_random_word()
+        self.canvas.itemconfig(self.card_background, image=self.card_front_img)
+        self.canvas.itemconfig(self.word,text=self.word_en,fill="black")
+        self.canvas.itemconfig(self.title,text="English",fill="black")
+        self.window.after(3000, func=self.flip_card)
+
+    def flip_card(self):
+
+        self.canvas.itemconfig(self.title,text="Franch",fill="white")
+        self.canvas.itemconfig(self.word,text=self.word_fr,fill="white")
+        self.canvas.itemconfig(self.card_background,image=self.card_back_ing)
 
 
 
--- pickwords.py
+++ pickwords.py
@@ -14,8 +14,10 @@ class Pick_word:
         self.word_fr = self.random_key =random.choice(list(self.word_dict.keys()))
         # 获取随机选择的键对应的值
         self.word_en = self.word_dict[self.random_key]
-        return self.word_en
+        # 返回法语单词和对应的英语单词
+        return self.word_fr, self.word_en
+        # return self.word_en
 
 
 # 获取随机选择的单词并打印
-print(Pick_word().pick_random_word())
+# print(Pick_word().pick_random_word())
